9bc32cbc35dc5d7032b48d89159e676be1e13ea7
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2023-10-09 21:21:13 +0200

    Updated path of the ram

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ def generate_template_upload(folder: str, version: str) -> None:
     # Add races
     for j in os.listdir(os.path.join(base_folder, "races")):
         race = json.load(open(os.path.join(base_folder, "races", j), 'r'))
-        race["source"] = "Tales from Ivory"
+        race["source"] = "TfI"
         base["race"].append(race)
     if False:
         for j in os.listdir(os.path.join(base_folder, "subraces")):
             subrace = json.load(open(os.path.join(base_folder, "subraces", j), 'r'))
-            subrace["source"] = "Tales from Ivory"
-            subrace["raceSource"] = "Tales from Ivory"
+            subrace["source"] = "TfI"
+            subrace["raceSource"] = "TfI"
             base["subrace"].append(subrace)
     # Add subclasses (TODO)
     for j in os.listdir(os.path.join(base_folder, "subclasses")):
@@ -34,12 +34,12 @@ def generate_template_upload(folder: str, version: str) -> None:
     # Add feats
     for j in os.listdir(os.path.join(base_folder, "feats")):
         feat = json.load(open(os.path.join(base_folder, "feats", j), 'r'))
-        feat["source"] = "iRdW"
+        feat["source"] = "TfI"
         base["feat"].append(feat)
     if False:
         for j in os.listdir(os.path.join(base_folder, "spells")):
             feat = json.load(open(os.path.join(base_folder, "spells", j), 'r'))
-            feat["source"] = "iRdW"
+            feat["source"] = "TfI"
             base["spell"].append(feat)
     json.dump(base, open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "ivory_compendium.json"), 'w', encoding='utf-8'), ensure_ascii=False)
 
